fe701f3c873af342a9597ebe052db05d5b441423
Nathan Franklin <nathan.franklin@gmail.com>
2026-03-30 16:03:28 -0500

    Use clearsessions command in cms step

--- conf/scripts/post-deploy.sh
+++ conf/scripts/post-deploy.sh
@@ -36,6 +36,6 @@ if [[ -n "$(docker ps -q -f name=^/portal_cms$)" && ("$service" == *"cms"* || "$
   docker exec portal_cms python3 manage.py migrate
   docker exec portal_cms python3 manage.py collectstatic --noinput --clear
 
-  # Delete all sessions to force users to log out after deployment
-  docker exec portal_cms python3 manage.py shell -c "from django.contrib.sessions.models import Session; Session.objects.all().delete()"
+  # Delete all expired sessions
+  docker exec portal_django python3 manage.py clearsessions
 fi
